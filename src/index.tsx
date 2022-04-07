a78ae913d0715c1cd6860ff425dbbac7d6cc09a7
KQT3 <kristoffer.pettersson@sensera.se>
2022-04-07 19:47:52 +0200

    README.md

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
